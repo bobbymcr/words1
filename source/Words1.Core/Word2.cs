afe8800189298c01978f7c4b3c29710388da9a42
Brian Rogers <bobbymcr@bobbymcr.com>
2013-08-15 23:03:41 -0700

    Add 2 letter word

---
+++ source/Words1.Core/Word2.cs
@@ -0,0 +1,86 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Word2.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Words1
+{
+    using System;
+
+    public struct Word2 : IComparable<Word2>, IEquatable<Word2>
+    {
+        private readonly char l1;
+        private readonly char l2;
+
+        public Word2(char l1, char l2)
+        {
+            this.l1 = l1;
+            this.l2 = l2;
+        }
+
+        public Word2(string word)
+        {
+            if (word == null)
+            {
+                throw new ArgumentNullException("word");
+            }
+
+            if (word.Length != 2)
+            {
+                throw new ArgumentException("Word must have exactly two letters.", "word");
+            }
+
+            this = new Word2(word[0], word[1]);
+        }
+
+        public char L1
+        {
+            get { return this.l1; }
+        }
+
+        public char L2
+        {
+            get { return this.l2; }
+        }
+
+        public override bool Equals(object obj)
+        {
+            bool isEqual = false;
+            if (obj is Word2)
+            {
+                isEqual = this.Equals((Word2)obj);
+            }
+
+            return isEqual;
+        }
+
+        public override int GetHashCode()
+        {
+            int l = this.l1 << 16;
+            l |= this.l2;
+            return l;
+        }
+
+        public override string ToString()
+        {
+            return string.Empty + this.l1 + this.l2;
+        }
+
+        public bool Equals(Word2 other)
+        {
+            return this.CompareTo(other) == 0;
+        }
+
+        public int CompareTo(Word2 other)
+        {
+            int c = this.l1.CompareTo(other.l1);
+            if (c != 0)
+            {
+                return c;
+            }
+
+            return this.l2.CompareTo(other.l2);
+        }
+    }
+}
---
+++ test/Words1.Test.Unit/Word2Test.cs
@@ -0,0 +1,139 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Word2Test.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Words1.Test.Unit
+{
+    using System;
+    using Xunit;
+
+    public class Word2Test
+    {
+        public Word2Test()
+        {
+        }
+
+        [Fact]
+        public void Construct_FromString_SetsPropertyValues()
+        {
+            Word2 c = new Word2("ab");
+            
+            Assert.Equal('a', c.L1);
+            Assert.Equal('b', c.L2);
+        }
+
+        [Fact]
+        public void Construct_FromChars_SetsPropertyValues()
+        {
+            Word2 c = new Word2('a', 'b');
+
+            Assert.Equal('a', c.L1);
+            Assert.Equal('b', c.L2);
+        }
+
+        [Fact]
+        public void Construct_WordTooLong_ThrowsArgument()
+        {
+            Exception e = Record.Exception(() => new Word2("long"));
+            Assert.NotNull(e);
+            ArgumentException ae = Assert.IsType<ArgumentException>(e);
+            Assert.Equal("word", ae.ParamName);
+        }
+
+        [Fact]
+        public void Construct_WordTooShort_ThrowsArgument()
+        {
+            Exception e = Record.Exception(() => new Word2("a"));
+            Assert.NotNull(e);
+            ArgumentException ae = Assert.IsType<ArgumentException>(e);
+            Assert.Equal("word", ae.ParamName);
+        }
+
+        [Fact]
+        public void Construct_NullWord_ThrowsArgumentNull()
+        {
+            string word = null;
+            Exception e = Record.Exception(() => new Word2(word));
+            Assert.NotNull(e);
+            ArgumentNullException ane = Assert.IsType<ArgumentNullException>(e);
+            Assert.Equal("word", ane.ParamName);
+        }
+
+        [Fact]
+        public void ToString_ReturnsWord()
+        {
+            Word2 word = new Word2("ab");
+
+            Assert.Equal("ab", word.ToString());
+        }
+
+        [Fact]
+        public void GetHashCode_UniqueUnlessValuesAreEqual()
+        {
+            Word2 one = new Word2("on");
+            Word2 oneA = new Word2("on");
+            Word2 two = new Word2("to");
+            Word2 twoA = new Word2("to");
+
+            int c1 = one.GetHashCode();
+            int c2 = two.GetHashCode();
+            int c1a = oneA.GetHashCode();
+            int c2a = twoA.GetHashCode();
+
+            Assert.Equal(c1, c1a);
+            Assert.Equal(c2, c2a);
+            Assert.NotEqual(c1, c2);
+        }
+
+        [Fact]
+        public void Equals_ComparesCorrectly()
+        {
+            Word2 one = new Word2("on");
+            Word2 oneA = new Word2("on");
+            Word2 two = new Word2("to");
+            Word2 twoA = new Word2("to");
+            object twoO = two;
+
+            Assert.True(one.Equals(oneA));
+            Assert.True(oneA.Equals(one));
+            Assert.True(two.Equals(twoA));
+            Assert.True(twoA.Equals(two));
+            Assert.True(two.Equals(twoO));
+
+            Assert.False(one.Equals(two));
+            Assert.False(two.Equals(one));
+            Assert.False(two.Equals(null));
+        }
+
+        [Fact]
+        public void CompareTo_ComparesCorrectly()
+        {
+            Word2 one = new Word2("aa");
+            Word2 two = new Word2("ab");
+            Word2 three = new Word2("ba");
+            Word2 four = new Word2("bb");
+
+            Assert.True(one.CompareTo(one) == 0);
+            Assert.True(one.CompareTo(two) < 0);
+            Assert.True(one.CompareTo(three) < 0);
+            Assert.True(one.CompareTo(four) < 0);
+
+            Assert.True(two.CompareTo(one) > 0);
+            Assert.True(two.CompareTo(two) == 0);
+            Assert.True(two.CompareTo(three) < 0);
+            Assert.True(two.CompareTo(four) < 0);
+
+            Assert.True(three.CompareTo(one) > 0);
+            Assert.True(three.CompareTo(two) > 0);
+            Assert.True(three.CompareTo(three) == 0);
+            Assert.True(three.CompareTo(four) < 0);
+
+            Assert.True(four.CompareTo(one) > 0);
+            Assert.True(four.CompareTo(two) > 0);
+            Assert.True(four.CompareTo(three) > 0);
+            Assert.True(four.CompareTo(four) == 0);
+        }
+    }
+}
