b9804167f6335c82e1d0bc96855af747d211b218
Brian Rogers <bobbymcr@bobbymcr.com>
2013-08-15 23:17:23 -0700

    Add Word2Grid

---
+++ source/Words1.Core/Word2Grid.cs
@@ -0,0 +1,95 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Word2Grid.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Words1
+{
+    using System;
+    using System.Text;
+
+    public struct Word2Grid : IEquatable<Word2Grid>
+    {
+        private readonly char a00;
+        private readonly char a01;
+        private readonly char a10;
+        private readonly char a11;
+
+        public Word2Grid(Word2 row1, Word2 row2)
+        {
+            this.a00 = row1.L1;
+            this.a01 = row1.L2;
+            this.a10 = row2.L1;
+            this.a11 = row2.L2;
+        }
+
+        public char A00
+        {
+            get { return this.a00; }
+        }
+
+        public char A01
+        {
+            get { return this.a01; }
+        }
+
+        public char A10
+        {
+            get { return this.a10; }
+        }
+
+        public char A11
+        {
+            get { return this.a11; }
+        }
+
+        public Word2 Row1
+        {
+            get { return new Word2(this.a00, this.a01); }
+        }
+
+        public Word2 Row2
+        {
+            get { return new Word2(this.a10, this.a11); }
+        }
+        
+        public Word2 Column1
+        {
+            get { return new Word2(this.a00, this.a10); }
+        }
+
+        public Word2 Column2
+        {
+            get { return new Word2(this.a01, this.a11); }
+        }
+
+        public Word2Grid Transpose()
+        {
+            return new Word2Grid(this.Column1, this.Column2);
+        }
+
+        public bool Equals(Word2Grid other)
+        {
+            return
+                (this.a00 == other.a00) &&
+                (this.a01 == other.a01) &&
+                (this.a10 == other.a10) &&
+                (this.a11 == other.a11);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            this.WriteBlock(b => sb.AppendLine(b));
+            sb.Remove(sb.Length - 2, 2);
+            return sb.ToString();
+        }
+
+        public void WriteBlock(Action<string> onText)
+        {
+            onText(this.Row1.ToString());
+            onText(this.Row2.ToString());
+        }
+    }
+}
---
+++ test/Words1.Test.Unit/Word2GridTest.cs
@@ -0,0 +1,81 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Word2GridTest.cs" company="Brian Rogers">
+// Copyright (c) Brian Rogers. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Words1.Test.Unit
+{
+    using System.Collections.Generic;
+    using Xunit;
+
+    public class Word2GridTest
+    {
+        public Word2GridTest()
+        {
+        }
+
+        [Fact]
+        public void Construct_FromRows_SetsPropertyValues()
+        {
+            Word2[] rows = new Word2[] { new Word2("ab"), new Word2("ef") };
+            Word2Grid grid = new Word2Grid(rows[0], rows[1]);
+
+            Assert.Equal('a', grid.A00);
+            Assert.Equal('b', grid.A01);
+            Assert.Equal('e', grid.A10);
+            Assert.Equal('f', grid.A11);
+
+            Assert.Equal(rows[0], grid.Row1);
+            Assert.Equal(rows[1], grid.Row2);
+
+            Assert.Equal(new Word2("ae"), grid.Column1);
+            Assert.Equal(new Word2("bf"), grid.Column2);
+        }
+
+        [Fact]
+        public void Equals_ComparesCorrectly()
+        {
+            Word2[] rows = new Word2[] { new Word2("ab"), new Word2("ef") };
+            Word2Grid grid1a = new Word2Grid(rows[0], rows[1]);
+            Word2Grid grid1b = new Word2Grid(rows[0], rows[1]);
+            Word2Grid grid2 = new Word2Grid(rows[1], rows[1]);
+
+            Assert.True(grid1a.Equals(grid1b));
+            Assert.True(grid1b.Equals(grid1a));
+            Assert.False(grid1b.Equals(grid2));
+            Assert.False(grid2.Equals(grid1a));
+        }
+
+        [Fact]
+        public void Transpose_CreatesRowAndColumnSwappedGrid()
+        {
+            Word2[] rows = new Word2[] { new Word2("ab"), new Word2("ef") };
+            Word2Grid grid = new Word2Grid(rows[0], rows[1]);
+
+            Word2Grid gridT = grid.Transpose();
+
+            Assert.Equal(new Word2("ae"), gridT.Row1);
+            Assert.Equal(new Word2("bf"), gridT.Row2);
+        }
+
+        [Fact]
+        public void ToString_OutputsRowsToString()
+        {
+            Word2Grid grid = new Word2Grid(new Word2("ab"), new Word2("ef"));
+
+            Assert.Equal("ab\r\nef", grid.ToString());
+        }
+
+        [Fact]
+        public void WriteBlocks_CallsFuncForEachBlock()
+        {
+            Word2Grid grid = new Word2Grid(new Word2("ab"), new Word2("ef"));
+
+            List<string> blocks = new List<string>();
+            grid.WriteBlock(b => blocks.Add(b));
+
+            Assert.Equal(new string[] { "ab", "ef" }, blocks.ToArray());
+        }
+    }
+}
